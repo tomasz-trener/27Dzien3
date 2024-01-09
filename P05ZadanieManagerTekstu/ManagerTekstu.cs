9f700492193181c33e7a79233e6e55255f433447
kurs <kurs@DESKTOP-V3HDKPQ>
2024-01-09 10:14:04 +0100

    q

---
+++ P05ZadanieManagerTekstu/ManagerTekstu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P05ZadanieManagerTekstu
+{
+    internal class ManagerTekstu
+    {
+        public string ZnajdzNajdluzszyWyraz(string zdanie)
+        {
+            if (zdanie.Length < 1)
+                throw new ArgumentException("Zdanie zbyt krótkie");
+
+
+            string[] wyrazy = zdanie.Split(' ');
+            string aktualnieNajdluzszy = string.Empty;
+            foreach (var w in wyrazy)
+            {
+                if (w.Length > aktualnieNajdluzszy.Length)
+                {
+                    aktualnieNajdluzszy = w;
+                }
+            }
+
+            return aktualnieNajdluzszy;
+        }
+
+        public string[] ZnajdzWszystkieNajdluzszeWyrazy(string zdanie)
+        {
+            if (string.IsNullOrWhiteSpace(zdanie))
+            {
+                throw new ArgumentException("Zdanie nie może być puste lub zawierać tylko białe znaki");
+            }
+
+            string najdluzszy = ZnajdzNajdluzszyWyraz(zdanie);
+            return znajdzWyrazyOPodajDlugosci(najdluzszy.Length, zdanie);
+        }
+
+        private string[] znajdzWyrazyOPodajDlugosci(int dlugosc, string zdanie)
+        {
+            HashSet<string> znalezione = new HashSet<string>(); // nie chcemy powtorzen 
+
+            foreach (var w in zdanie.Split(' '))
+            {
+                if (w.Length == dlugosc)
+                {
+                    znalezione.Add(w);
+                }
+            }
+
+            return znalezione.ToArray();
+        }
+ 
+    }
+}
--- P05ZadanieManagerTekstu/Program.cs
+++ P05ZadanieManagerTekstu/Program.cs
@@ -10,6 +10,8 @@ namespace P05ZadanieManagerTekstu
     {
         static void Main(string[] args)
         {
+
+
         }
     }
 }
