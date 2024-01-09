2e13658e6585a242b5663668490d63becef82ad4
kurs <kurs@DESKTOP-V3HDKPQ>
2024-01-09 11:57:49 +0100

    q

---
+++ P08Wlasciwosci/Czlowiek.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P08Wlasciwosci
+{
+    internal class Czlowiek
+    {
+        public string Nazwisko { get; set; } // Właściwość 
+
+        private string imie; // pole 
+
+        private int wzrost; // domyślna wartość to 0
+
+        public string Imie
+        {
+            get
+            {
+                return imie.ToUpper();
+            }
+            set
+            {
+                imie = value;
+            }
+        }
+
+        public string ImieNazwisko
+        {
+            get
+            {
+                string s1 = imie.ToUpper();
+                string s2 = Nazwisko.ToUpper();
+
+                return s1 + " " + s2;
+            }
+            set
+            {
+                string[] s= value.Split(' ');
+                imie = s[0];
+                Nazwisko = s[1];
+            }
+        }
+
+
+        // przykłądowa metoda:
+        public void Idz(int ileMetrow)
+        {
+
+        }
+
+        public string PrzedstawSie()
+        {
+            return "Nazywam sie" + imie + " " + Nazwisko;
+        }
+
+    }
+}
---
+++ P08Wlasciwosci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P08Wlasciwosci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kalkulator kalkulator = new Kalkulator();
+            kalkulator.Kolor = "czerwony";
+
+            Console.WriteLine(kalkulator.Kolor);
+
+            // wcześniej już korzystaliśmy z właściowości 
+            List<string> lista = new List<string>();
+            int liczbaElementow = lista.Count;
+            string s = "ala ma kota";
+            int dlugosc = s.Length;
+
+
+            Czlowiek czlowiek = new Czlowiek();
+
+            czlowiek.Nazwisko = "Kowalski";
+
+            czlowiek.Imie = "Jan";
+
+            Console.WriteLine(czlowiek.Imie); //JAN
+
+            Console.WriteLine(czlowiek.ImieNazwisko);
+
+            czlowiek.ImieNazwisko = "Adam nowak";
+        }
+    }
+}
