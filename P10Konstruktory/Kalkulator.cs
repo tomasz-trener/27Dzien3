a95c737bb1edcaa2deb53de422bff8cf8cd05e12
kurs <kurs@DESKTOP-V3HDKPQ>
2024-01-09 13:36:23 +0100

    q

---
+++ P10Konstruktory/Kalkulator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10Konstruktory
+{
+    internal class Kalkulator
+    {
+
+		private string _tryb; // dobra konwencja to dodawanie podkreślenia  przy polach prywatnch 
+
+        private string kolor;
+
+		public string Tryb
+		{
+			get { return _tryb; }
+			set { _tryb = value; }
+		}
+
+		public string Policz(double a, double b)
+		{
+			if (_tryb == "naukowy")
+			{
+				return string.Format("{0:0000}", a / b);
+			}
+            if (_tryb == "normalny")
+            {
+                return string.Format("{0:00}", a / b);
+            }
+
+			throw new Exception("Tryb kalkulatora nieznany");
+        }
+
+        public Kalkulator(string tryb)
+        {
+			_tryb = tryb;
+        }
+
+        public Kalkulator(string tryb, string kolor)
+        {
+            _tryb = tryb;
+            this.kolor = kolor;
+        }
+
+    }
+}
---
+++ P10Konstruktory/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10Konstruktory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kalkulator k = new Kalkulator("naukowy");
+
+            //k.Tryb = "naukowy";
+
+            string wynik = k.Policz(10, 3);
+
+
+            ManagerPogody mp = new ManagerPogody(Jednostka.Farenheit);
+
+            double temp1 = mp.PodajTemperature("warszawa");
+            double temp2 = mp.PodajTemperature("kraków");
+
+            Console.ReadKey();
+        }
+    }
+}
